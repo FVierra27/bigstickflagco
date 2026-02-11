3c33c3f61d0632751c34c1e338988029c1c4ca51
FVierra27 <fvierra27@gmail.com>
2026-02-11 07:30:45 -0800

    Docker-compose update

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -1,22 +1,16 @@
-import { loadEnv, defineConfig } from "@medusajs/framework/utils"
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
-loadEnv(process.env.NODE_ENV || "development", process.cwd())
+loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    redisUrl: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    },
-  },
-
-  // ✅ backend can disable serving admin, but admin image can still build it
-  admin: {
-    disable: process.env.MEDUSA_DISABLE_ADMIN === "true",
-  },
+    }
+  }
 })
